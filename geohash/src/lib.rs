cb159161606ca47404c139a178153612db527dc2
Daniel Rammer <hamersaw@bushpath.com>
2019-05-14 20:03:50 -0600

    returning errors from geohash encode_16 and decode_16 functions.

--- geohash/src/lib.rs
+++ geohash/src/lib.rs
@@ -1,13 +1,22 @@
+use std::io::{Error, ErrorKind, Result};
+
 static CODES_16: &'static [char; 16] =
     &['0', '1', '2', '3', '4', '5', '6', '7',
     '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 static MASKS_16: &'static [usize; 4] = &[1, 2, 4, 8];
 
-pub fn encode_16(latitude: f32, longitude: f32, length: usize) -> String {
+pub fn encode_16(latitude: f32, longitude: f32, length: usize)
+        -> Result<String> {
     let mut geohash = String::with_capacity(length);
     let (mut min_lat, mut max_lat, mut min_long, mut max_long) =
         (-90f32, 90f32, -180f32, 180f32);
     let mut index;
+
+    if latitude < min_lat || latitude > max_lat {
+        return Err(Error::new(ErrorKind::InvalidData, "invalid latitude"));
+    } else if longitude < min_long || longitude > max_long {
+        return Err(Error::new(ErrorKind::InvalidData, "invalid longitude"));
+    }
 
     for _ in 0..length {
         index = 0;
@@ -35,10 +44,10 @@ pub fn encode_16(latitude: f32, longitude: f32, length: usize) -> String {
         geohash.push(CODES_16[index]);
     }
 
-    geohash
+    Ok(geohash)
 }
 
-pub fn decode_16(geohash: &str) -> (f32, f32, f32, f32) {
+pub fn decode_16(geohash: &str) -> Result<(f32, f32, f32, f32)> {
     let (mut min_lat, mut max_lat, mut min_long, mut max_long) =
         (-90f32, 90f32, -180f32, 180f32);
     let mut index;
@@ -47,7 +56,8 @@ pub fn decode_16(geohash: &str) -> (f32, f32, f32, f32) {
         index = match c as usize {
             x if x >= 48 && x <= 57 => x - 48,
             x if x >= 97 && x <= 102 => x - 87,
-            _ => 0, // TODO - throw error
+            _ => return Err(Error::new(
+                ErrorKind::InvalidData, "invalid character")),
         };
 
         for i in 0..4 {
@@ -71,7 +81,7 @@ pub fn decode_16(geohash: &str) -> (f32, f32, f32, f32) {
         }
     }
 
-    (min_lat, max_lat, min_long, max_long)
+    Ok((min_lat, max_lat, min_long, max_long))
 }
 
 #[cfg(test)]
@@ -88,8 +98,10 @@ mod tests {
     fn test_coordinates(coord: (f32, f32),
             min_length: usize, max_length: usize) {
         for i in min_length..max_length {
-            let encoded = super::encode_16(coord.0, coord.1, i);
-            let decoded = super::decode_16(&encoded);
+            let encoded = super::encode_16(coord.0, coord.1, i)
+                .expect("encoding geohash");
+            let decoded = super::decode_16(&encoded)
+                .expect("decoding geohash");
 
             assert!(decoded.0 <= coord.0 && coord.0 <= decoded.1);
             assert!(decoded.2 <= coord.1 && coord.1 <= decoded.3);
